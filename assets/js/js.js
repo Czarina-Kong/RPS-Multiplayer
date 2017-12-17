7d09afb431768521334ae0518d533df0a66c189b
zeelee28 <32179557+zeelee28@users.noreply.github.com>
2017-12-16 16:53:45 -0800

    update html and link firebase

---
+++ assets/js/js.js
@@ -0,0 +1,12 @@
+// Initialize Firebase
+var config = {
+	apiKey: "AIzaSyCMSACWVbrYcx3oU9u77F6UV1R33mc99YE",
+	authDomain: "recentuser-3706d.firebaseapp.com",
+	databaseURL: "https://recentuser-3706d.firebaseio.com",
+	projectId: "recentuser-3706d",
+	storageBucket: "recentuser-3706d.appspot.com",
+	messagingSenderId: "1092227535335"
+};
+firebase.initializeApp(config)
+
+
